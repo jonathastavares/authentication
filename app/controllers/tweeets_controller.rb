b94add814fe13318620827985d95ca25a3305709
jonathastavares <jonathastavares@id.uff.br>
2021-05-07 00:46:26 -0300

    Changed some CSS styles for new layout

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -27,7 +27,7 @@ class TweeetsController < ApplicationController
 
     respond_to do |format|
       if @tweeet.save
-        format.html { redirect_to root_path, notice: "Tweeet was successfully created." }
+        format.html { redirect_to root_path, notice: "Secret was successfully created." }
         format.json { render :show, status: :created, location: @tweeet }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,10 +39,11 @@ class TweeetsController < ApplicationController
   # PATCH/PUT /tweeets/1 or /tweeets/1.json
   def update
     respond_to do |format|
-      if @tweeet.update(tweeet_params)
-        format.html { redirect_to @tweeet, notice: "Tweeet was successfully updated." }
+      if current_user == @tweeet.user && @tweeet.update(tweeet_params)
+        format.html { redirect_to root_path, notice: "Secret was successfully updated." }
         format.json { render :show, status: :ok, location: @tweeet }
       else
+        format.html { redirect_to root_path, alert: "You don't have permissions to edit this Secret." }
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @tweeet.errors, status: :unprocessable_entity }
       end
@@ -53,7 +54,7 @@ class TweeetsController < ApplicationController
   def destroy
     @tweeet.destroy
     respond_to do |format|
-      format.html { redirect_to tweeets_url, notice: "Tweeet was successfully destroyed." }
+      format.html { redirect_to tweeets_url, notice: "Secret was successfully destroyed." }
       format.json { head :no_content }
     end
   end
